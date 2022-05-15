8e0715e3fc578c08ace5d96ef70c307af6b0985b
Anton Atanasov <anton.atanasov@consensys.net>
2022-05-15 17:02:17 +0300

    Update host to deployed application

--- src/app/services/db.js
+++ src/app/services/db.js
@@ -1,33 +1,10 @@
-const ticket = {
-    id: "Ticket 1",
-    type: "General Support",
-    state: "In Progress",
-    commState: "Waiting for a reply",
-    submittedBy: "0x71C7656EC7ab88b098defB751B7401B5f6d8976F",
-    description: "Cannot Buy NFT",
-  };
-
-  const tickets = [
-    ticket,
-    ticket,
-    ticket,
-    ticket,
-    ticket,
-    ticket,
-    ticket,
-    ticket,
-    ticket,
-  ];
-
 const hosturl = 'http://localhost:3000/api';
 
 export async function loadTicketsForOwner(owner) {
-    console.log('waiting');
     const path = `${hosturl}/tickets`;
 
     const test = await fetch(path);
     const parsed = await test.json();
-    console.log(parsed);
 
     return parsed;
 }
@@ -47,7 +24,6 @@ export async function respondToTicket(ticketId, message, user) {
 
     if (res.status === 201) {
       const result = await res.json();
-      debugger;
       return result;
     }
 
@@ -58,8 +34,4 @@ export async function respondToTicket(ticketId, message, user) {
     return null;
   }
 
-}
-
-export async function updateTicket() {
-  // await appendMessage(project.address, info.address, info.address, request.body.ticket, request.body.message)
 }
