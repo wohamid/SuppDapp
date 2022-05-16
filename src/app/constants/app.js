22b8b7b460c6962c06872ab1f96f7e615f01c2e5
Marta Johnson <martajohnson@Martas-MacBook-Pro.local>
2022-05-16 12:07:29 -0500

    Uses origin for backend

--- src/app/constants/app.js
+++ src/app/constants/app.js
@@ -1,6 +1,5 @@
 export const domain = window.location.host;
 export const origin = window.location.origin;
 
-// export const BACKEND_ADDR = `${origin}/api`;
-export const BACKEND_ADDR = new URL(process.env.BACKEND_HOST).href
+export const BACKEND_ADDR = new URL(origin).href
 console.log('BACKEND_ADDR', BACKEND_ADDR);
